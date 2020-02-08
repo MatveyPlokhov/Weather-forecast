e254b3d3f37b64f36d9ac2c63525dfbcbac3f233
mapl <matvey.pavlovich@yandex.ru>
2020-02-08 16:54:55 +0300

    Прогноз погоды по местоположению

--- app/src/main/java/com/mapl/weather_forecast/LocationWeatherActivity.java
+++ app/src/main/java/com/mapl/weather_forecast/LocationWeatherActivity.java
@@ -12,8 +12,10 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.mapl.weather_forecast.adapter.ViewPagerAdapter;
@@ -34,7 +36,8 @@ import retrofit2.Response;
 
 public class LocationWeatherActivity extends AppCompatActivity {
     private ViewPager2 viewPager;
-    private LocationManager locationManager = null;
+    private LocationManager locManager = null;
+    private LocListener locListener = null;
     private CoordinatorLayout coordinatorLayout;
 
     @Override
@@ -44,10 +47,21 @@ public class LocationWeatherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_weather);
 
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         initToolbar();
         initView();
         getLocation();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        if (locListener != null) locManager.removeUpdates(locListener);
+        super.onPause();
     }
 
     private void initToolbar() {
@@ -69,58 +83,66 @@ public class LocationWeatherActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
                     Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
         } else {
-            @SuppressLint("MissingPermission") final Location loc = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+            if (locListener == null) locListener = new LocListener();
+            locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    3000L, 1.0F, locListener);
+
+            @SuppressLint("MissingPermission") final Location loc = locManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
 
             if (loc != null) {
-                OpenWeatherMap.getSingleton().getAPI().loadWeather(
-                        String.valueOf(loc.getLatitude()), String.valueOf(loc.getLongitude()),
-                        "8b872d9c30ed844f1fa73c7172a8313f", "metric", Locale.getDefault().getLanguage())
-                        .enqueue(new Callback<OpenWeatherMapModel>() {
-                            @Override
-                            public void onResponse(Call<OpenWeatherMapModel> call, Response<OpenWeatherMapModel> response) {
-                                List<CurrentWeather> list = new ArrayList<>();
-                                CurrentWeather currentWeather = new CurrentWeather();
-                                OpenWeatherMapModel model = response.body();
-                                if (response.isSuccessful() && model != null) {
-                                    currentWeather.location = " ";
-                                    currentWeather.latitude = loc.getLatitude();
-                                    currentWeather.longitude = loc.getLongitude();
-                                    currentWeather.weatherId = model.weather[0].id;
-                                    currentWeather.sunrise = model.sys.sunrise * 1000;
-                                    currentWeather.sunset = model.sys.sunset * 1000;
-                                    currentWeather.description = model.weather[0].description;
-                                    currentWeather.temperature = Math.round(model.main.temp);
-                                    currentWeather.feelsLike = Math.round(model.main.feels_like);
-                                    currentWeather.pressure = model.main.pressure;
-                                    currentWeather.humidity = model.main.humidity;
-                                    list.add(currentWeather);
-                                    ViewPagerAdapter adapter = ViewPagerAdapter.getInstance(LocationWeatherActivity.this, list);
-                                    adapter.refreshData(list);
-                                    viewPager.setAdapter(adapter);
-                                    viewPager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
-                                    viewPager.setClipToPadding(false);
-                                    viewPager.setClipChildren(false);
-                                    viewPager.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
-                                } else {
-                                    HashMap<String, String> connectionError = new HashMap<>();
-                                    connectionError.put("Location", currentWeather.location +
-                                            " (" + currentWeather.latitude + ", " + currentWeather.longitude + ")");
-                                    connectionError.put(String.valueOf(response.code()), response.message());
-                                    Analytics.trackEvent("Error in getting weather data",
-                                            connectionError, Flags.CRITICAL);
-                                    Snackbar.make(coordinatorLayout,response.code() + " " + response.message() ,
-                                            Snackbar.LENGTH_SHORT).show();
-                                }
-                            }
-
-                            @Override
-                            public void onFailure(Call<OpenWeatherMapModel> call, Throwable t) {
-                                Snackbar.make(coordinatorLayout,getResources().getString(R.string.weather_data_false),
-                                    Snackbar.LENGTH_LONG).show();
-                            }
-                        });
+                setWeather(loc);
             }
         }
+    }
+
+    private void setWeather(final Location loc) {
+        OpenWeatherMap.getSingleton().getAPI().loadWeather(
+                String.valueOf(loc.getLatitude()), String.valueOf(loc.getLongitude()),
+                "8b872d9c30ed844f1fa73c7172a8313f", "metric", Locale.getDefault().getLanguage())
+                .enqueue(new Callback<OpenWeatherMapModel>() {
+                    @Override
+                    public void onResponse(Call<OpenWeatherMapModel> call, Response<OpenWeatherMapModel> response) {
+                        List<CurrentWeather> list = new ArrayList<>();
+                        CurrentWeather currentWeather = new CurrentWeather();
+                        OpenWeatherMapModel model = response.body();
+                        if (response.isSuccessful() && model != null) {
+                            currentWeather.location = " ";
+                            currentWeather.latitude = loc.getLatitude();
+                            currentWeather.longitude = loc.getLongitude();
+                            currentWeather.weatherId = model.weather[0].id;
+                            currentWeather.sunrise = model.sys.sunrise * 1000;
+                            currentWeather.sunset = model.sys.sunset * 1000;
+                            currentWeather.description = model.weather[0].description;
+                            currentWeather.temperature = Math.round(model.main.temp);
+                            currentWeather.feelsLike = Math.round(model.main.feels_like);
+                            currentWeather.pressure = model.main.pressure;
+                            currentWeather.humidity = model.main.humidity;
+                            list.add(currentWeather);
+                            ViewPagerAdapter adapter = ViewPagerAdapter.getInstance(LocationWeatherActivity.this, list);
+                            adapter.refreshData(list);
+                            viewPager.setAdapter(adapter);
+                            viewPager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+                            viewPager.setClipToPadding(false);
+                            viewPager.setClipChildren(false);
+                            viewPager.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+                        } else {
+                            HashMap<String, String> connectionError = new HashMap<>();
+                            connectionError.put("Location", currentWeather.location +
+                                    " (" + currentWeather.latitude + ", " + currentWeather.longitude + ")");
+                            connectionError.put(String.valueOf(response.code()), response.message());
+                            Analytics.trackEvent("Error in getting weather data",
+                                    connectionError, Flags.CRITICAL);
+                            Snackbar.make(coordinatorLayout, response.code() + " " + response.message(),
+                                    Snackbar.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<OpenWeatherMapModel> call, Throwable t) {
+                        Snackbar.make(coordinatorLayout, getResources().getString(R.string.weather_data_false),
+                                Snackbar.LENGTH_LONG).show();
+                    }
+                });
     }
 
     @Override
@@ -129,6 +151,27 @@ public class LocationWeatherActivity extends AppCompatActivity {
             boolean permissionsGranted = (grantResults.length > 1 && grantResults[1] == PackageManager.PERMISSION_GRANTED)
                     && (grantResults[0] == PackageManager.PERMISSION_GRANTED);
             if (permissionsGranted) recreate();
+        }
+    }
+
+    private final class LocListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.i("Location", "локация изменилась: " + location.toString());
+            setWeather(location);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
         }
     }
 }
